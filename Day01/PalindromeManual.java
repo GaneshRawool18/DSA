2cf18fcc10b4b66c03d3478fe20b0d1c24647212
GaneshRawool18 <ganeshrawool07@gmail.com>
2025-06-17 02:54:03 +0530

    Added manual palindrome check code using String and loop

--- Day01/PalindromeManual.java
+++ Day01/PalindromeManual.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 
-class PalindromeManual {
-    public static void main(String[] args) {
+class PalindromeManual{
+public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         //Taking input from user
-        System.out.print("Enter a string : ");
+        System.out.print("Enter a String : ");
         String originalString = sc.nextLine();
 
         String reverseString = "";
 
         // Manually reverse the String using charAt()
-        for(int i = originalString.length() - 1; i>=0 ;i--){
+        for(int i=originalString.length() - 1;i>=0;i--){
             reverseString += originalString.charAt(i);
         }
 
