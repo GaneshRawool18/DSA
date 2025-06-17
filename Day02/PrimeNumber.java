121d590871cfd9222e4fc7b4dcf2bbd2926a09bc
GaneshRawool18 <ganeshrawool07@gmail.com>
2025-06-17 15:02:39 +0530

    feat: add optimized prime number checker using square root method

---
+++ Day02/PrimeNumber.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Taking input from user 
+        System.out.print("Enter a number : ");
+        int number = sc.nextInt();
+
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
+
+    // Optimized method to check for prime
+    static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        if (n == 2) return true;
+        if (n % 2 == 0) return false;
+        int sqrt = (int)Math.sqrt(n);
+        for (int i = 3; i <= sqrt; i += 2) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+}
